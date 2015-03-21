1d2539fb67f3d0c41e9cdaf8177d260597a6bd15
Nate McCord <mccordnate@gmail.com>
2015-03-21 14:24:51 -0500

    Time Picker Dialog

--- app/src/main/java/vandyhacks/dios/hsphuc/healthystart/AlarmsActivity.java
+++ app/src/main/java/vandyhacks/dios/hsphuc/healthystart/AlarmsActivity.java
@@ -1,10 +1,19 @@
 package vandyhacks.dios.hsphuc.healthystart;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import vandyhacks.dios.hsphuc.healthystart.Models.Alarm;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import vandyhacks.dios.hsphuc.healthystart.Models.AlarmManager;
 
@@ -38,8 +47,28 @@ public class AlarmsActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if(id == R.id.action_add_alarm){
+            openTimePicker();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openTimePicker() {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, selectedHour);
+                calendar.set(Calendar.MINUTE, selectedMinute);
+            }
+        }, hour, minute, false);
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
     }
 }
